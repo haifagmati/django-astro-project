98eecf7d419c4eb1fbe4831c246d956bc14e2e3b
Your Name <you@example.com>
2022-12-05 13:54:08 +0100

    chore:split settings.py file

---
+++ astro_project/settings/developement.py
@@ -0,0 +1,22 @@
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+from .base import *
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    "default":{
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
+}
