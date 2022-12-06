271b64f2e022e25626a3fa600ab4087550090380
Your Name <you@example.com>
2022-12-06 10:07:51 +0100

    feat:add custom user model

--- astro_project/urls.py
+++ astro_project/urls.py
@@ -6,3 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Astro Project Admin"
+admin.site.site_title = "Astro Project Admin Portal"
+admin.site.index_title = "Welcome to the Astro Project Portal"
